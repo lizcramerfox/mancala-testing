7b5d390dcac7099368d7abb4118353962b7a207f
lizcramerfox <ecfox2008@gmail.com>
2022-01-04 16:41:46 -0500

    Solves failure on Start New Game (moves db reset to beforeEach)

--- mancala-testing/cypress/integration/mancala-tests/mancala.specs.js
+++ mancala-testing/cypress/integration/mancala-tests/mancala.specs.js
@@ -2,11 +2,8 @@
 
 describe('mancala tests', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:7165')
-  })
-
-  afterEach(() => {
     cy.cleanupApp()
+    cy.visit('http://localhost:7165')
   })
 
   it('opens landing page', () => {
@@ -39,59 +36,42 @@ describe('mancala tests', () => {
     cy.contains('Success')
   })
 
-  // it('changes password', () => {
-  //   // Signs up
-  //   cy.get('[href="#sign-up"]').click()
-  //   cy.get('#email').type('test@mancala.com')
-  //   cy.get('#password').type('a')
-  //   cy.get('#passwordConfirmation').type('a')
-  //   cy.get('[type="submit"]').click()
-  //   cy.contains('Success')
-  //   cy.get('[href="#sign-out"]').click()
+  it('changes password', () => {
+    // Signs Up/In
+    cy.signUp()
+    cy.signIn()
 
-  //   // Signs in
-  //   cy.get('[href="#sign-in"]').click()
-  //   cy.get('#email').type('test@mancala.com')
-  //   cy.get('#password').type('a')
-  //   cy.get('[type="submit"]').click()
-  //   cy.contains('Success')
-  //   cy.contains('Welcome')
+    // Changes password
+    cy.get('[href="#change-password"]').click()
+    cy.get('#oldPassword').type('a')
+    cy.get('#newPassword').type('b')
+    cy.get('[value="Submit"]').click()
 
-  //   // Changes password
-  //   cy.get('[href="#change-password"]').click()
-  //   cy.get('#oldPassword').type('a')
-  //   cy.get('#newPassword').type('b')
-  //   cy.get('[value="Submit"]').click()
+    // Signs out
+    cy.signOut()
 
-  //   // Signs out
-  //   cy.get('[href="#sign-out"]').click()
-  //   cy.contains('Success')
+    // Signs in with new password
+    cy.get('[href="#sign-in"]').click()
+    cy.get('#email').type('test@mancala.com')
+    cy.get('#password').type('b')
+    cy.get('[value="Submit"]').click()
+    cy.contains('Success')
+    cy.contains('Welcome')
 
-  //   // Signs in with new password
-  //   cy.get('[href="#sign-in"]').click()
-  //   cy.get('#email').type('test@mancala.com')
-  //   cy.get('#password').type('b')
-  //   cy.get('[value="Submit"]').click()
-  //   cy.contains('Success')
-  //   cy.contains('Welcome')
+    // Changes password back to original
+    cy.get('[href="#change-password"]').click()
+    cy.get('#oldPassword').type('b')
+    cy.get('#newPassword').type('a')
+    cy.get('[value="Submit"]').click()
+    cy.contains('Success')
+  })
 
-  //   // Changes password back to original
-  //   cy.get('[href="#change-password"]').click()
-  //   cy.get('#oldPassword').type('b')
-  //   cy.get('#newPassword').type('a')
-  //   cy.get('[value="Submit"]').click()
-  // })
-
-  // it('starts a new game', () => {
-  //   cy.get('[href="#sign-in"]').click()
-  //   cy.get('#email').type('test@mancala.com')
-  //   cy.get('#password').type('a')
-  //   cy.get('[type="submit"]').click()
-  //   cy.contains('Success')
-  //   cy.contains('Welcome')
-  //   cy.get('.new-game').click()
-  //   cy.contains('Success')
-  // })
+  it('starts a new game', () => {
+    cy.signUp()
+    cy.signIn()
+    cy.get('.new-game').click()
+    cy.contains('Success')
+  })
 
   // it('deletes a new game', () => {
   //   cy.get('[href="#sign-in"]').click()
