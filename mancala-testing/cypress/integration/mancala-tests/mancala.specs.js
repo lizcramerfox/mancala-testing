e6690ec4b7172e67d4a3334b940818b43b1cf21b
lizcramerfox <ecfox2008@gmail.com>
2022-01-10 17:35:27 -0500

    Adds full game play-through (needs work at end of test); Deletes test examples

--- mancala-testing/cypress/integration/mancala-tests/mancala.specs.js
+++ mancala-testing/cypress/integration/mancala-tests/mancala.specs.js
@@ -10,112 +10,203 @@ describe('mancala tests', () => {
     cy.cleanupApp()
   })
 
-  it('opens landing page', () => {
-    cy.contains('Sign In')
-  })
+//   it('opens landing page', () => {
+//     cy.contains('Sign In')
+//   })
 
-  it('signs up', () => {
-    cy.get('[href="#sign-up"]').click()
-    cy.get('#email').type('test@mancala.com')
-    cy.get('#password').type('a')
-    cy.get('#passwordConfirmation').type('a')
-    cy.get('[type="submit"]').click()
-    cy.contains('Success')
-    cy.get('[href="#sign-out"]').click()
-  })  
+//   it('signs up', () => {
+//     cy.get('[href="#sign-up"]').click()
+//     cy.get('#email').type('test@mancala.com')
+//     cy.get('#password').type('a')
+//     cy.get('#passwordConfirmation').type('a')
+//     cy.get('[type="submit"]').click()
+//     cy.contains('Success')
+//     cy.get('[href="#sign-out"]').click()
+//   })  
 
-  it('signs in and signs out', () => {
-    cy.signUp()
+//   it('signs in and signs out', () => {
+//     cy.signUp()
     
-    // Signs In
-    cy.get('[href="#sign-in"]').click()
-    cy.get('#email').type('test@mancala.com')
-    cy.get('#password').type('a')
-    cy.get('[type="submit"]').click()
-    cy.contains('Success')
-    cy.contains('Welcome')
+//     // Signs In
+//     cy.get('[href="#sign-in"]').click()
+//     cy.get('#email').type('test@mancala.com')
+//     cy.get('#password').type('a')
+//     cy.get('[type="submit"]').click()
+//     cy.contains('Success')
+//     cy.contains('Welcome')
 
-    // Signs Out
-    cy.get('[href="#sign-out"]').click()
-    cy.contains('Success')
-  })
+//     // Signs Out
+//     cy.get('[href="#sign-out"]').click()
+//     cy.contains('Success')
+//   })
 
-  it('changes password', () => {
+//   it('changes password', () => {
+//     cy.signUp()
+//     cy.signIn()
+
+//     // Changes password
+//     cy.get('[href="#change-password"]').click()
+//     cy.get('#oldPassword').type('a')
+//     cy.get('#newPassword').type('b')
+//     cy.get('[value="Submit"]').click()
+
+//     // Signs out
+//     cy.signOut()
+
+//     // Signs in with new password
+//     cy.get('[href="#sign-in"]').click()
+//     cy.get('#email').type('test@mancala.com')
+//     cy.get('#password').type('b')
+//     cy.get('[value="Submit"]').click()
+//     cy.contains('Success')
+//     cy.contains('Welcome')
+
+//     // Changes password back to original
+//     cy.get('[href="#change-password"]').click()
+//     cy.get('#oldPassword').type('b')
+//     cy.get('#newPassword').type('a')
+//     cy.get('[value="Submit"]').click()
+//     cy.contains('Success')
+//   })
+
+//   it('starts a new game and then deletes it', () => {
+//     cy.signUp()
+//     cy.signIn()
+//     cy.get('.new-game').click()
+//     cy.contains('Success')
+//     cy.get('.delete-button').click()
+//     cy.get('.button-container > :nth-child(2)').click()
+//     cy.contains('Success')
+//   })
+
+//   it('starts a new game, makes a move', () => {
+//     cy.signUp()
+//     cy.signIn()
+//     cy.get('.new-game').click()
+    
+//     // Makes a move
+//     cy.get('#A5').click()
+    
+//     // Checks board
+//     cy.get('#A0').contains(4)
+//     cy.get('#A1').contains(4)
+//     cy.get('#A2').contains(4)
+//     cy.get('#A3').contains(4)
+//     cy.get('#A4').contains(4)
+//     cy.get('#A5').contains(0)
+//     cy.get('#AM').contains(1)
+//     cy.get('#B0').contains(5)
+//     cy.get('#B1').contains(5)
+//     cy.get('#B2').contains(5)
+//     cy.get('#B3').contains(4)
+//     cy.get('#B3').contains(4)
+//     cy.get('#B3').contains(4)
+//     cy.get('#BM').contains(0)
+//   })
+
+//   it('views saved game in index', () => {
+//     cy.signUp()
+//     cy.signIn()
+//     cy.get('.new-game').click()
+    
+//     cy.makeFirstMove()
+
+//     // View game in index
+//     cy.get('[href="#games"]').click()
+//     cy.get('.game-status-label').contains('IN PROGRESS')
+//     cy.get('.game-preview').contains('Player B\'s Turn')
+//     cy.get('.stones-a').contains('1')
+//     cy.get('.stones-b').contains('0')
+//   })
+// })
+
+  // it('completes a full game', () => {
+  //   cy.signUp()
+  //   cy.signIn()
+  //   cy.get('.new-game').click()
+
+  //   // Play complete game
+  //   cy.get('#A0').click()
+  //   cy.get('#B0').click()
+  //   cy.get('#A1').click()
+  //   cy.get('#A2').click()
+  //   cy.get('#B0').click()
+  //   cy.get('#A3').click()
+  //   cy.get('#B4').click()
+  //   cy.get('#A0').click()
+  //   cy.get('#B0').click()
+  //   cy.get('#A4').click()
+  //   cy.get('#B4').click()
+  //   cy.get('#A1').click()
+  //   cy.get('#B1').click()
+  //   cy.get('#A3').click()
+  //   cy.get('#B0').click()
+  //   cy.get('#A0').click()
+  //   cy.get('#B2').click()
+  //   cy.get('#A2').click()
+  //   cy.get('#B4').click()
+  //   cy.get('#A1').click()
+  //   cy.get('#B5').click()
+  //   cy.get('#A3').click()
+  //   cy.get('#A4').click()
+  //   cy.get('#B0').click()
+  //   cy.get('#A0').click()
+  //   cy.get('#B1').click()
+  //   cy.get('#A2').click()
+  //   cy.get('#B2').click()
+  //   cy.get('#A1').click()
+  //   cy.get('#B3').click()
+  //   cy.get('#A4').click()
+  //   cy.get('#A5').click()
+  //   cy.get('#A3').click()
+  //   cy.get('#A5').click()
+  //   cy.get('#A2').click()
+  //   cy.get('#B5').click()
+  //   cy.get('#B4').click()
+
+  //   // Check for correct winner
+  //   cy.get('#AM').contains(21)
+  //   cy.get('#BM').contains(27)
+  //   cy.get('.game-info').contains('Player B Wins')
+  // })
+
+  // it('views a complete game in index', () => {
+  //   cy.signUp()
+  //   cy.signIn()
+  //   cy.get('.new-game').click()
+  //   cy.playCompleteGame()
+
+  //   // Returns to index view
+  //   cy.get('[href="#games"]').click()
+
+  //   // Checks game stats
+  //   cy.get('.game-status-label').contains('GAME OVER')
+  //   cy.get('.game-preview').contains('Player B Wins')
+  //   cy.get('.stones-a').contains('21')
+  //   cy.get('.stones-b').contains('27')
+  // })
+
+  it('views a complete game and an incomplete game in index', () => {
     cy.signUp()
     cy.signIn()
-
-    // Changes password
-    cy.get('[href="#change-password"]').click()
-    cy.get('#oldPassword').type('a')
-    cy.get('#newPassword').type('b')
-    cy.get('[value="Submit"]').click()
-
-    // Signs out
-    cy.signOut()
-
-    // Signs in with new password
-    cy.get('[href="#sign-in"]').click()
-    cy.get('#email').type('test@mancala.com')
-    cy.get('#password').type('b')
-    cy.get('[value="Submit"]').click()
-    cy.contains('Success')
-    cy.contains('Welcome')
-
-    // Changes password back to original
-    cy.get('[href="#change-password"]').click()
-    cy.get('#oldPassword').type('b')
-    cy.get('#newPassword').type('a')
-    cy.get('[value="Submit"]').click()
-    cy.contains('Success')
-  })
-
-  it('starts a new game and then deletes it', () => {
-    cy.signUp()
-    cy.signIn()
+    
+    // Plays incomplete game
     cy.get('.new-game').click()
-    cy.contains('Success')
-    cy.get('.delete-button').click()
-    cy.get('.button-container > :nth-child(2)').click()
-    cy.contains('Success')
-  })
-
-  it('starts a new game, makes a move', () => {
-    cy.signUp()
-    cy.signIn()
-    cy.get('.new-game').click()
-    
-    // Makes a move
-    cy.get('#A5').click()
-    
-    // Checks board
-    cy.get('#A0').contains(4)
-    cy.get('#A1').contains(4)
-    cy.get('#A2').contains(4)
-    cy.get('#A3').contains(4)
-    cy.get('#A4').contains(4)
-    cy.get('#A5').contains(0)
-    cy.get('#AM').contains(1)
-    cy.get('#B0').contains(5)
-    cy.get('#B1').contains(5)
-    cy.get('#B2').contains(5)
-    cy.get('#B3').contains(4)
-    cy.get('#B3').contains(4)
-    cy.get('#B3').contains(4)
-    cy.get('#BM').contains(0)
-  })
-
-  it('views saved game in index', () => {
-    cy.signUp()
-    cy.signIn()
-    cy.get('.new-game').click()
-    
     cy.makeFirstMove()
 
-    // View game in index
+    // Plays complete game
+    cy.get('[href="#games-create"]').click()
+    cy.playCompleteGame()
+
+
+    // Returns to index view
     cy.get('[href="#games"]').click()
-    cy.get('.game-status-label').contains('IN PROGRESS')
-    cy.get('.game-preview').contains('Player B\'s Turn')
-    cy.get('.stones-a').contains('1')
-    cy.get('.stones-b').contains('0')
+
+    // Checks game data
+    cy.get('.index').children().should('have.length', 2)
+    
+    //// NEEDS FIXING: FIGURE OUT HOW TO SELECT INDIVIDUAL CHILDREN IN DIV
+    // cy.get('.index>a').eq(0).click()
+    // cy.get('.index' nth-child(0)]'').contains('GAME OVER')
   })
 })
