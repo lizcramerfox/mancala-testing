5fd760bc6f5e4c08114060fa3da09abc6ad749e9
lizcramerfox <ecfox2008@gmail.com>
2022-02-02 15:16:59 -0500

    Removes unused useEffect import from AuthPage

--- client/src/AuthPage/AuthPage.js
+++ client/src/AuthPage/AuthPage.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useEffect } from 'react'
+import React, { useContext, useState } from 'react'
 import { AuthContext } from '../Context/context'
 import { signIn, signOut } from '../api/auth'
 
