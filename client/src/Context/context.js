2691f396c272514ae4f22e27e95f5cdd61c7790a
lizcramerfox <ecfox2008@gmail.com>
2022-02-09 17:06:49 -0500

    Cleans up unused Auth.js folder and removes extra variables from context.js

--- client/src/Context/context.js
+++ client/src/Context/context.js
@@ -4,10 +4,6 @@ export const AuthContext = createContext({
   token: null,
   user: null,
   confirmChange: false,
-  showLogin: false,
-  showLogout: false,
-  showSignUp: false,
-  showChangePassword: false,
   login: () => {}, 
   logout: () => {}
 })
